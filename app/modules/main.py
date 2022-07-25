bbdbd9b39133ef7c7e43fbd1efcbe9e81af1e6f4
tam0201 <dominhtam.0201@gmail.com>
2022-07-26 01:58:15 +0700

    FastAPI v2.0 release

--- app/modules/main.py
+++ app/modules/main.py
@@ -8,6 +8,9 @@ app=FastAPI()
 
 app.include_router(kubeflow_router.router, prefix="/kubeflow", tags=["kubeflow API"])
 
+@app.get("/")
+def index():
+    return {"data": "Application ran successfully - FastAPI"}
 
 if __name__=='__main__':
     uvicorn.run(app, host="0.0.0.0", port=port)
