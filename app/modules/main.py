b4a15e7cc584324f618b9ad562f5eac3ae5096a5
tam0201 <dominhtam.0201@gmail.com>
2022-07-26 09:20:36 +0700

    error

--- app/modules/main.py
+++ app/modules/main.py
@@ -13,4 +13,4 @@ def index():
     return {"data": "Application ran successfully - FastAPI"}
 
 if __name__=='__main__':
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port, reload=False)
